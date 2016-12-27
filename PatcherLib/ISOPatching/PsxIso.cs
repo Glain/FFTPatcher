2d6f9105966332b3d079a921656b2430116be4aa
Glain <glainmail8@gmail.com>
2016-12-26 21:21:02 -0500

    Added EVENT_ETC_OUT to possible files for XML patches

--- PatcherLib/ISOPatching/PsxIso.cs
+++ PatcherLib/ISOPatching/PsxIso.cs
@@ -270,8 +270,9 @@ namespace PatcherLib.Iso
             public static KeyValuePair<int, byte[]> EVENT_REQUIRE_OUT = new KeyValuePair<int, byte[]>(0, new byte[4] { 0x57, 0x41, 0x49, 0x54 });
             public static KeyValuePair<int, byte[]> EVENT_EQUIP_OUT = new KeyValuePair<int, byte[]>(0, new byte[4] { 0x25, 0x64, 0x00, 0x44 });
             public static KeyValuePair<int, byte[]> EVENT_OPTION_OUT = new KeyValuePair<int, byte[]>(0x50, new byte[4] { 0xac, 0x5f, 0xb5, 0x26 });
+            public static KeyValuePair<int, byte[]> EVENT_ETC_OUT = new KeyValuePair<int, byte[]>(0, new byte[4] { 0x57, 0x41, 0x49, 0x54 });
             public static KeyValuePair<int, byte[]> EVENT_HELPMENU_OUT = new KeyValuePair<int, byte[]>(0x10, new byte[4] { 0x04, 0x75, 0xa5, 0x24 });
-
+            
             public static KeyValuePair<int, byte[]> Get(PsxIso.Sectors sector)
             {
                 return (KeyValuePair<int, byte[]>)typeof(FileCheckValue).GetField(sector.ToString()).GetValue(null);
@@ -288,6 +289,7 @@ namespace PatcherLib.Iso
         	OFFSET_EVENT_REQUIRE_OUT = 0x1BF000,
             OFFSET_EVENT_EQUIP_OUT = 0x1BF000,
             OFFSET_EVENT_OPTION_OUT = 0x1BF000,
+            OFFSET_EVENT_ETC_OUT = 0x1BF000,
             OFFSET_EVENT_HELPMENU_OUT = 0x1DF000
         }
 
