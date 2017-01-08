30da77ef6a208b825f7015bd738411198d7edf38
Glain <glainmail8@gmail.com>
2017-01-08 13:30:45 -0500

    Include MassHexASM when staging builds

--- staging.bat
+++ staging.bat
@@ -3,6 +3,7 @@ xcopy /s bin\x86\Release staging
 xcopy /s FFTacText\bin\x86\Release\*.* staging
 xcopy /s FFTorgASM\bin\x86\Release\*.* staging
 xcopy /s ShishiSpriteEditor\bin\x86\Release\*.* staging
+xcopy /s MassHexASM\bin\Release\*.* staging
 del staging\*.pdb
 del staging\*.vshost.exe
 del staging\*.vshost.exe.config
