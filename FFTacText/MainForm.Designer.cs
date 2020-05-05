2eec3588ffac6f8218d09d1b480993f186e943d3
Glain <glainmail8@gmail.com>
2020-05-05 13:34:43 -0400

    FFTactext main form designer file VS re-arrange

--- FFTacText/MainForm.Designer.cs
+++ FFTacText/MainForm.Designer.cs
@@ -59,6 +59,8 @@ namespace FFTPatcher.TextEditor
             this.fileMenuItem = new System.Windows.Forms.MenuItem();
             this.openMenuItem = new System.Windows.Forms.MenuItem();
             this.saveMenuItem = new System.Windows.Forms.MenuItem();
+            this.generateResourcesZipMenuItem = new System.Windows.Forms.MenuItem();
+            this.separatorGenerateResourcesZip = new System.Windows.Forms.MenuItem();
             this.exitMenuItem = new System.Windows.Forms.MenuItem();
             this.isoMenuItem = new System.Windows.Forms.MenuItem();
             this.importPsxIsoMenuItem = new System.Windows.Forms.MenuItem();
@@ -68,17 +70,15 @@ namespace FFTPatcher.TextEditor
             this.helpMenuItem = new System.Windows.Forms.MenuItem();
             this.allowedSymbolsMenuItem = new System.Windows.Forms.MenuItem();
             this.aboutMenuItem = new System.Windows.Forms.MenuItem();
-            this.generateResourcesZipMenuItem = new System.Windows.Forms.MenuItem();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.patchPsxBackgroundWorker = new System.ComponentModel.BackgroundWorker();
-            this.fileEditor1 = new FFTPatcher.TextEditor.Editors.FileEditor();
             this.progressBar = new PatcherLib.Controls.ProgressBarWithText();
+            this.fileEditor1 = new FFTPatcher.TextEditor.Editors.FileEditor();
             separator2 = new System.Windows.Forms.MenuItem();
             separator3 = new System.Windows.Forms.MenuItem();
             separator4 = new System.Windows.Forms.MenuItem();
             separator5 = new System.Windows.Forms.MenuItem();
-            System.Windows.Forms.MenuItem separatorGenerateResourcesZip = new System.Windows.Forms.MenuItem();
             this.SuspendLayout();
             // 
             // separator2
@@ -100,11 +100,6 @@ namespace FFTPatcher.TextEditor
             // 
             separator5.Index = 3;
             separator5.Text = "-";
-            //
-            // separatorGenerateResourcesZip
-            //
-            separatorGenerateResourcesZip.Index = 4;
-            separatorGenerateResourcesZip.Text = "-";
             // 
             // mainMenu
             // 
@@ -122,7 +117,7 @@ namespace FFTPatcher.TextEditor
             this.saveMenuItem,
             separator2,
             this.generateResourcesZipMenuItem,
-            separatorGenerateResourcesZip,
+            this.separatorGenerateResourcesZip,
             this.exitMenuItem});
             this.fileMenuItem.Text = "File";
             // 
@@ -136,12 +131,17 @@ namespace FFTPatcher.TextEditor
             this.saveMenuItem.Enabled = false;
             this.saveMenuItem.Index = 1;
             this.saveMenuItem.Text = "&Save .ffttext...";
-            //
+            // 
             // generateResourcesZipMenuItem
-            //
+            // 
             this.generateResourcesZipMenuItem.Enabled = false;
             this.generateResourcesZipMenuItem.Index = 3;
             this.generateResourcesZipMenuItem.Text = "Generate Resources.zip";
+            // 
+            // separatorGenerateResourcesZip
+            // 
+            this.separatorGenerateResourcesZip.Index = 4;
+            this.separatorGenerateResourcesZip.Text = "-";
             // 
             // exitMenuItem
             // 
@@ -206,6 +206,18 @@ namespace FFTPatcher.TextEditor
             this.patchPsxBackgroundWorker.WorkerReportsProgress = true;
             this.patchPsxBackgroundWorker.WorkerSupportsCancellation = true;
             // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.BackColor = System.Drawing.Color.White;
+            this.progressBar.ForeColor = System.Drawing.Color.Blue;
+            this.progressBar.Location = new System.Drawing.Point(12, 367);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(754, 23);
+            this.progressBar.TabIndex = 3;
+            this.progressBar.Visible = false;
+            // 
             // fileEditor1
             // 
             this.fileEditor1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -213,26 +225,14 @@ namespace FFTPatcher.TextEditor
             | System.Windows.Forms.AnchorStyles.Right)));
             this.fileEditor1.Location = new System.Drawing.Point(13, 13);
             this.fileEditor1.Name = "fileEditor1";
-            this.fileEditor1.Size = new System.Drawing.Size(753, 398);
+            this.fileEditor1.Size = new System.Drawing.Size(753, 377);
             this.fileEditor1.TabIndex = 4;
-            // 
-            // progressBar
-            // 
-            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar.BackColor = System.Drawing.Color.White;
-            this.progressBar.ForeColor = System.Drawing.Color.Blue;
-            this.progressBar.Location = new System.Drawing.Point(12, 388);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(754, 23);
-            this.progressBar.TabIndex = 3;
-            this.progressBar.Visible = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 423);
+            this.ClientSize = new System.Drawing.Size(778, 402);
             this.Controls.Add(this.fileEditor1);
             this.Controls.Add(this.progressBar);
             this.Menu = this.mainMenu;
@@ -263,6 +263,7 @@ namespace FFTPatcher.TextEditor
         private System.Windows.Forms.MenuItem menuItem2;
         private System.Windows.Forms.MenuItem importPspIsoMenuItem;
         private System.Windows.Forms.MenuItem generateResourcesZipMenuItem;
+        private System.Windows.Forms.MenuItem separatorGenerateResourcesZip;
 
     }
 }
