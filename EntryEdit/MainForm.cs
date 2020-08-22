ead58f9344ecb8d77b2e95fac9de1313c3b1dad1
Glain <glainmail8@gmail.com>
2020-08-22 11:41:51 -0400

    EntryEdit update (primarily data)

--- EntryEdit/MainForm.cs
+++ EntryEdit/MainForm.cs
@@ -17,6 +17,22 @@ namespace EntryEdit
             InitializeComponent();
         }
 
+        private void SaveXMLFromEventFilenames(string inputFilepath, string outputFilepath)
+        {
+            string[] filepaths = System.IO.Directory.GetFiles(inputFilepath);
+            StringBuilder sb = new StringBuilder("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+            sb.Append(Environment.NewLine);
+            sb.AppendLine("<Entries>");
+            for (int index = 0; index < filepaths.Length; index++)
+            {
+                string filename = System.IO.Path.GetFileName(filepaths[index]);
+                string entryName = filename.Substring(8, filename.IndexOf('.') - 8).Replace("&", "and");
+                sb.AppendFormat("    <Entry name=\"{0}\" />{1}", entryName, Environment.NewLine);
+            }
+            sb.AppendLine("</Entries>");
+            System.IO.File.WriteAllText(outputFilepath, sb.ToString());
+        }
+
         private void menuItem_Exit_Click(object sender, EventArgs e)
         {
             Close();
