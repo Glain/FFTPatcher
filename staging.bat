45577e25cb8bbf4e3cde1f7f68e04c9d1f0e5922
Glain <glainmail8@gmail.com>
2020-09-02 18:25:09 -0400

    Build updates (CPU target, build directories)

--- staging.bat
+++ staging.bat
@@ -1,10 +1,10 @@
-del /Q staging\*.*
-xcopy /s /Y bin\x86\Release staging
-xcopy /s /Y FFTacText\bin\x86\Release\*.* staging
-xcopy /s /Y FFTorgASM\bin\x86\Release\*.* staging
-xcopy /s /Y ShishiSpriteEditor\bin\x86\Release\*.* staging
+rmdir /s /q staging
+mkdir staging
+xcopy /s /Y bin\Release staging
+xcopy /s /Y FFTacText\bin\Release\*.* staging
+xcopy /s /Y FFTorgASM\bin\Release\*.* staging
+xcopy /s /Y ShishiSpriteEditor\bin\Release\*.* staging
 del staging\*.pdb
 del staging\*.vshost.exe
 del staging\*.vshost.exe.config
 del staging\*.manifest
-
