8d41fe2e382d464db6ec690bed1088173952fb6e
Glain <glainmail8@gmail.com>
2020-09-08 10:31:57 -0400

    Build configuration

--- staging.bat
+++ staging.bat
@@ -1,9 +1,9 @@
 rmdir /s /q staging
 mkdir staging
-xcopy /s /Y bin\Release staging
-xcopy /s /Y FFTacText\bin\Release\*.* staging
-xcopy /s /Y FFTorgASM\bin\Release\*.* staging
-xcopy /s /Y ShishiSpriteEditor\bin\Release\*.* staging
+xcopy /s /Y bin\x86\Release staging
+xcopy /s /Y FFTacText\bin\x86\Release\*.* staging
+xcopy /s /Y FFTorgASM\bin\x86\Release\*.* staging
+xcopy /s /Y ShishiSpriteEditor\bin\x86\Release\*.* staging
 del staging\*.pdb
 del staging\*.vshost.exe
 del staging\*.vshost.exe.config
