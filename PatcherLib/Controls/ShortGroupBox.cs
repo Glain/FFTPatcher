4fceda5b400bdc113951710f5c125ec49f9341ce
Glain <glainmail8@gmail.com>
2020-09-08 22:24:20 -0400

    Created ShortGroupBox in PatcherLib

---
+++ PatcherLib/Controls/ShortGroupBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PatcherLib.Controls
+{
+    public class ShortGroupBox : GroupBox
+    {
+        const int HeightReductionAmount = 12;
+
+        public override Size GetPreferredSize(Size proposedSize)
+        {
+            Size size = base.GetPreferredSize(proposedSize);
+            return new Size(size.Width, size.Height - HeightReductionAmount);
+        }
+    }
+}
