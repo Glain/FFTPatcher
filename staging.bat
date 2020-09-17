7f5f0077199b8cbb8cddf2d37752934b088f5eaa
Glain <glainmail8@gmail.com>
2020-09-17 17:03:32 -0400

    Staging batch file update to include MassHexASM

--- staging.bat
+++ staging.bat
@@ -5,6 +5,7 @@ xcopy /s /Y bin\x86\Release staging
 xcopy /s /Y FFTacText\bin\x86\Release\*.* staging
 xcopy /s /Y FFTorgASM\bin\x86\Release\*.* staging
 xcopy /s /Y ShishiSpriteEditor\bin\x86\Release\*.* staging
+xcopy /s /Y MassHexASM\bin\x86\Release\*.* staging
 del staging\*.pdb
 del staging\*.vshost.exe
 del staging\*.vshost.exe.config
