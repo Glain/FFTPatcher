43d71b3a434070c2fcdaf147f70c4dc569b9776d
Glain <glainmail8@gmail.com>
2020-09-27 16:12:19 -0400

    EntryEdit main form expansion

--- EntryEdit/MainForm.Designer.cs
+++ EntryEdit/MainForm.Designer.cs
@@ -45,6 +45,9 @@
             this.menuItem_LoadScript = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItem_SaveScript = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItem_File_Separator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuItem_LoadAllScripts = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem_SaveAllScripts = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem_File_Separator4 = new System.Windows.Forms.ToolStripSeparator();
             this.menuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItem_Edit = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItem_Copy = new System.Windows.Forms.ToolStripMenuItem();
@@ -54,9 +57,6 @@
             this.menuItem_RestoreDefaults = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItem_View = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItem_CheckSize = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuItem_LoadAllScripts = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuItem_SaveAllScripts = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuItem_File_Separator4 = new System.Windows.Forms.ToolStripSeparator();
             this.tabControl.SuspendLayout();
             this.tabPage_BattleConditionals.SuspendLayout();
             this.tabPage_WorldConditionals.SuspendLayout();
@@ -75,7 +75,7 @@
             this.tabControl.Location = new System.Drawing.Point(0, 27);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(960, 791);
+            this.tabControl.Size = new System.Drawing.Size(1000, 791);
             this.tabControl.TabIndex = 0;
             // 
             // tabPage_BattleConditionals
@@ -84,7 +84,7 @@
             this.tabPage_BattleConditionals.Location = new System.Drawing.Point(4, 22);
             this.tabPage_BattleConditionals.Name = "tabPage_BattleConditionals";
             this.tabPage_BattleConditionals.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_BattleConditionals.Size = new System.Drawing.Size(952, 765);
+            this.tabPage_BattleConditionals.Size = new System.Drawing.Size(992, 765);
             this.tabPage_BattleConditionals.TabIndex = 0;
             this.tabPage_BattleConditionals.Text = "Battle Conditionals";
             this.tabPage_BattleConditionals.UseVisualStyleBackColor = true;
@@ -96,7 +96,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.battleConditionalSetsEditor.Location = new System.Drawing.Point(4, 4);
             this.battleConditionalSetsEditor.Name = "battleConditionalSetsEditor";
-            this.battleConditionalSetsEditor.Size = new System.Drawing.Size(947, 762);
+            this.battleConditionalSetsEditor.Size = new System.Drawing.Size(987, 762);
             this.battleConditionalSetsEditor.TabIndex = 0;
             // 
             // tabPage_WorldConditionals
@@ -149,7 +149,7 @@
             this.menuItem_View});
             this.menuBar.Location = new System.Drawing.Point(0, 0);
             this.menuBar.Name = "menuBar";
-            this.menuBar.Size = new System.Drawing.Size(964, 24);
+            this.menuBar.Size = new System.Drawing.Size(1004, 24);
             this.menuBar.TabIndex = 1;
             this.menuBar.Text = "menuStrip1";
             // 
@@ -225,6 +225,27 @@
             this.menuItem_File_Separator3.Name = "menuItem_File_Separator3";
             this.menuItem_File_Separator3.Size = new System.Drawing.Size(161, 6);
             // 
+            // menuItem_LoadAllScripts
+            // 
+            this.menuItem_LoadAllScripts.Enabled = false;
+            this.menuItem_LoadAllScripts.Name = "menuItem_LoadAllScripts";
+            this.menuItem_LoadAllScripts.Size = new System.Drawing.Size(164, 22);
+            this.menuItem_LoadAllScripts.Text = "Load All Scripts...";
+            this.menuItem_LoadAllScripts.Click += new System.EventHandler(this.menuItem_LoadAllScripts_Click);
+            // 
+            // menuItem_SaveAllScripts
+            // 
+            this.menuItem_SaveAllScripts.Enabled = false;
+            this.menuItem_SaveAllScripts.Name = "menuItem_SaveAllScripts";
+            this.menuItem_SaveAllScripts.Size = new System.Drawing.Size(164, 22);
+            this.menuItem_SaveAllScripts.Text = "Save All Scripts...";
+            this.menuItem_SaveAllScripts.Click += new System.EventHandler(this.menuItem_SaveAllScripts_Click);
+            // 
+            // menuItem_File_Separator4
+            // 
+            this.menuItem_File_Separator4.Name = "menuItem_File_Separator4";
+            this.menuItem_File_Separator4.Size = new System.Drawing.Size(161, 6);
+            // 
             // menuItem_Exit
             // 
             this.menuItem_Exit.Name = "menuItem_Exit";
@@ -296,32 +317,11 @@
             this.menuItem_CheckSize.Text = "Check Size";
             this.menuItem_CheckSize.Click += new System.EventHandler(this.menuItem_CheckSize_Click);
             // 
-            // menuItem_LoadAllScripts
-            // 
-            this.menuItem_LoadAllScripts.Enabled = false;
-            this.menuItem_LoadAllScripts.Name = "menuItem_LoadAllScripts";
-            this.menuItem_LoadAllScripts.Size = new System.Drawing.Size(164, 22);
-            this.menuItem_LoadAllScripts.Text = "Load All Scripts...";
-            this.menuItem_LoadAllScripts.Click += new System.EventHandler(this.menuItem_LoadAllScripts_Click);
-            // 
-            // menuItem_SaveAllScripts
-            // 
-            this.menuItem_SaveAllScripts.Enabled = false;
-            this.menuItem_SaveAllScripts.Name = "menuItem_SaveAllScripts";
-            this.menuItem_SaveAllScripts.Size = new System.Drawing.Size(164, 22);
-            this.menuItem_SaveAllScripts.Text = "Save All Scripts...";
-            this.menuItem_SaveAllScripts.Click += new System.EventHandler(this.menuItem_SaveAllScripts_Click);
-            // 
-            // menuItem_File_Separator4
-            // 
-            this.menuItem_File_Separator4.Name = "menuItem_File_Separator4";
-            this.menuItem_File_Separator4.Size = new System.Drawing.Size(161, 6);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(964, 821);
+            this.ClientSize = new System.Drawing.Size(1004, 821);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.menuBar);
             this.MainMenuStrip = this.menuBar;
