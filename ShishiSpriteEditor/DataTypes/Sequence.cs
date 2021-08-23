b518e195afc8e61c10ff75b52b28a659ad925494
Glain <glainmail8@gmail.com>
2021-08-23 19:04:11 -0400

    Fix static initialization issue in Sequence (Shishi)

--- ShishiSpriteEditor/DataTypes/Sequence.cs
+++ ShishiSpriteEditor/DataTypes/Sequence.cs
@@ -17,6 +17,25 @@ namespace FFTPatcher.SpriteEditor
 
         private Set<int> uniqueFrames;
 
+        private static Dictionary<byte, int> _jumps = null;
+        public static Dictionary<byte, int> Jumps
+        {
+            get
+            {
+                if (_jumps == null)
+                {
+                    _jumps = new Dictionary<byte, int> {
+                        { 192, 1 },{ 198, 1 },{ 211, 1 },{ 212, 1 },{ 214, 1 },
+                        { 215, 1 },{ 216, 1 },{ 226, 1 },{ 238, 1 },{ 239, 1 },
+                        { 240, 1 },{ 246, 1 },{ 193, 2 },{ 217, 2 },{ 242, 2 },
+                        { 252, 2 },{ 250, 3 } 
+                    };
+                }
+
+                return _jumps;
+            }
+        }
+
         public static readonly IDictionary<SpriteType, IList<Sequence>> Sequences = GetSequences();
 
         private static IDictionary<SpriteType, IList<Sequence>> GetSequences()
@@ -44,7 +63,7 @@ namespace FFTPatcher.SpriteEditor
             return new ReadOnlyDictionary<SpriteType, IList<Sequence>>( sequences );
         }
 
-        public static IList<Sequence> BuildSequences( IList<byte> bytes, IList<string> names,bool mon )
+        public static IList<Sequence> BuildSequences( IList<byte> bytes, IList<string> names, bool mon )
         {
             List<UInt32> offsets = new List<uint>();
             for ( int i = 0; i < 0x100; i++ )
@@ -116,12 +135,6 @@ namespace FFTPatcher.SpriteEditor
             }
         }
 
-        private static Dictionary<byte, int> jumps = new Dictionary<byte, int> {
-                { 192, 1 },{ 198, 1 },{ 211, 1 },{ 212, 1 },{ 214, 1 },
-                { 215, 1 },{ 216, 1 },{ 226, 1 },{ 238, 1 },{ 239, 1 },
-                { 240, 1 },{ 246, 1 },{ 193, 2 },{ 217, 2 },{ 242, 2 },
-                { 252, 2 },{ 250, 3 } };
-
         private static IList<AnimationFrame> ProcessSequence( IList<byte> bytes, int number )
         {
             int i = 0;
@@ -132,9 +145,9 @@ namespace FFTPatcher.SpriteEditor
                 {
                     sequence.Add( new byte[2] { bytes[i], bytes[i + 1] } );
                 }
-                else if ( jumps.ContainsKey( bytes[i + 1] ) )
+                else if (Jumps.ContainsKey(bytes[i + 1]))
                 {
-                    i += jumps[bytes[i + 1]];
+                    i += Jumps[bytes[i + 1]];
                 }
 
                 i += 2;
