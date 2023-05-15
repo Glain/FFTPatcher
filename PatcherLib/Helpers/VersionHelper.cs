e6566912fc3665be08877d27dd7f346a06b37d50
Glain <glainmail8@gmail.com>
2023-05-15 17:52:56 -0400

    Version number update (0.497.0)

--- PatcherLib/Helpers/VersionHelper.cs
+++ PatcherLib/Helpers/VersionHelper.cs
@@ -8,7 +8,7 @@ namespace PatcherLib.Helpers
     public class VersionHelper
     {
         public const int VersionFirst = 0;
-        public const int VersionSecond = 496;
+        public const int VersionSecond = 497;
         public const int VersionThird = 0;
 
         public static readonly string VersionString = VersionFirst + "." + VersionSecond + "." + VersionThird;
